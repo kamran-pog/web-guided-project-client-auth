a4f8629d053f83a90a784e82e1bea68f09ca1d55
kamran-pog <kamran.k0807@gmail.com>
2023-04-01 12:57:32 -0700

    logout is not wokring

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React from 'react';
+import { Redirect } from 'react-router-dom';
 
-const Logout = ()=> {
-    return(<div></div>);
-}
+function Logout() {
+  const logout = () => {
+    localStorage.removeItem('token');
+    this.props.history.push('/login');
+  };
+
+  
+    return (
+      <div>
+        {/* if (localStorage.getItem("token")) {
+
+        } */}
+        <h5>You are about logout!! ARE YOU SURE??</h5>
+        <button onClick={logout} >Logout</button>
+      </div>
+    );
+  }
+
 
 export default Logout;
+
